bfcbba9ae48bd00de52531b6d0664a9e0c193a49
mauricio <Mauricio@iMac-de-Mauricio.local>
2016-05-22 12:32:47 -0500

    cambie el html para la pagina.

--- misitio/misitio/urls.py
+++ misitio/misitio/urls.py
@@ -3,8 +3,10 @@ from django.contrib import admin
 
 urlpatterns = [
     # Examples:
-    # url(r'^$', 'misitio.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
+    #url(r'^$', 'misitio.views.home', name='home'),
+    #url(r'^blog/', include('blog.urls')),
+    
 
     url(r'^admin/', include(admin.site.urls)),
+    url(r'', include('blog.urls')),
 ]
